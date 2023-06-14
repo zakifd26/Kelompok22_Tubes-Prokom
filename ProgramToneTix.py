b8dc4e389a4a1fa430fa74a4cf62eb027a8aa9bd
zakifd26 <127032824+zakifd26@users.noreply.github.com>
2023-06-14 17:20:00 +0700

    Program ToneTix

--- ProgramToneTix.py
+++ ProgramToneTix.py
@@ -2,3 +2,18 @@ import csv
 import random
 import sys
 from datetime import datetime
+
+def welcome_message():
+    print("==============================================================")
+    print("                  SELAMAT DATANG DI TONETIX!")
+    print("      Kami adalah platform penjualan tiket konser terbaik untukmu!")
+    print("==============================================================")
+    print("Tonetix menyediakan tiket konser untuk berbagai genre musik populer,")
+    print("dengan kualitas suara dan pengalaman konser yang tak terlupakan.")
+    print("Dengan Tonetix, Anda dapat menjelajahi dan membeli tiket untuk")
+    print("konser-konser terbaik dari musisi dan band favorit Anda.")
+    print("==============================================================")
+    print("Untuk membeli tiket, silakan pilih nomor genre musik yang Anda minati.")
+    print("Kami akan membantu Anda mendapatkan pengalaman konser yang luar biasa!")
+    print("==============================================================")
+    print()
